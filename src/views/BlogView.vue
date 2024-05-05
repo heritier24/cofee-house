5def66e70077f0674f840c2243ff50bf4b1249f3
heritier24 <heritier.ganza@kudibooks.com>
2024-05-05 17:35:09 +0200

    blog component initiated well

---
+++ src/views/BlogView.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="header_section header_bg">
+    <div class="container">
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"
+          ><img src="images/logo.png"
+        />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="coffee">Coffees</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="/blog">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <div class="login_bt">
+              <ul>
+                <li>
+                  <a href="#"
+                    ><span class="user_icon"
+                      ><i class="fa fa-user" aria-hidden="true"></i></span
+                    >Login</a
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    ><i class="fa fa-search" aria-hidden="true"></i
+                  ></a>
+                </li>
+              </ul>
+            </div>
+          </form>
+        </div>
+      </nav>
+    </div>
+  </div>
+  <!-- header section end -->
+  <!-- blog section start -->
+  <div class="blog_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="about_taital">Our Blog</h1>
+        </div>
+      </div>
+      <div class="blog_section_2">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="blog_box">
+              <div class="blog_img"><img src="images/blog-img1.png" /></div>
+              <h4 class="date_text">05 April</h4>
+              <h4 class="prep_text">PREP TECHNIQUES Coffee</h4>
+              <p class="lorem_text">
+                distracted by the readable content of a page when looking at its
+                layout. The point of using Lorem Ipsum is that it has a
+                moredistracted by the readable content of a page when looking at
+                its layout. The point of using Lorem Ipsum is that it has a more
+              </p>
+            </div>
+            <div class="read_btn"><a href="#">Read More</a></div>
+          </div>
+          <div class="col-md-6">
+            <div class="blog_box">
+              <div class="blog_img"><img src="images/blog-img2.png" /></div>
+              <h4 class="date_text">05 April</h4>
+              <h4 class="prep_text">Enjoy Best Coffee</h4>
+              <p class="lorem_text">
+                distracted by the readable content of a page when looking at its
+                layout. The point of using Lorem Ipsum is that it has a
+                moredistracted by the readable content of a page when looking at
+                its layout. The point of using Lorem Ipsum is that it has a more
+              </p>
+            </div>
+            <div class="read_btn"><a href="#">Read More</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- blog section end -->
+  <!-- footer section start -->
+ <div class="footer_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="footer_social_icon">
+            <ul>
+              <li>
+                <a href="#"
+                  ><i class="fa fa-facebook" aria-hidden="true"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+              </li>
+              <li>
+                <a href="#"
+                  ><i class="fa fa-linkedin" aria-hidden="true"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#"
+                  ><i class="fa fa-instagram" aria-hidden="true"></i
+                ></a>
+              </li>
+            </ul>
+          </div>
+          <div class="location_text">
+            <ul>
+              <li>
+                <a href="#">
+                  <i class="fa fa-phone" aria-hidden="true"></i
+                  ><span class="padding_left_10">0789326245</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa fa-envelope" aria-hidden="true"></i
+                  ><span class="padding_left_10">demo@gmail.com</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="form-group">
+            <textarea
+              class="update_mail"
+              placeholder="Your Email"
+              rows="5"
+              id="comment"
+              name="Your Email"
+            ></textarea>
+            <div class="subscribe_bt">
+              <a href="#"
+                ><i class="fa fa-arrow-right" aria-hidden="true"></i
+              ></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- footer section end -->
+  <!-- copyright section start -->
+  <div class="copyright_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <p class="copyright_text">
+            2024 All Rights Reserved. Design by
+            <a href="#">Heritier Ganza Tamba</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
